c20ee263b52cdc98e229e63c6e3d9cffdc453375
Israel Lopes <yisrael.lopes@gmail.com>
2020-03-26 03:58:49 -0300

    fix carousel

--- index.js
+++ index.js
@@ -1,41 +1,45 @@
+/**
+ * Creating home page carousel
+ */
 (function createCarousel () {
+
+  var areaRoot = document.querySelector('.area-root');
+  var carousel = document.createElement('ul');
+  carousel.setAttribute('class', 'carousel');
 
   /**
    * Create carousel card
    * @param {Object} item 
    */
-  var createCard = (item) => {
-    var areaRoot = document.querySelector('.area-root');
-      var carousel = document.createElement('ul');
-      carousel.setAttribute('class', 'carousel');
+  var createCard = (item, index) => {
+    var carouselCard = document.createElement('li');
+    carouselCard.setAttribute('class', 'item');
+    carouselCard.setAttribute('id', item.id);
 
-      var carouselCard = document.createElement('li');
-      carouselCard.setAttribute('class', 'item');
+    var image = document.createElement('img');
+    image.setAttribute('src', item.image);
+    carouselCard.appendChild(image);
 
-      var image = document.createElement('img');
-      image.setAttribute('src', item.image);
-      carouselCard.appendChild(image);
+    var name = document.createElement('span');
+    name.setAttribute('class', 'product-details')
+    name.innerText = item.name;
+    carouselCard.appendChild(name);
 
-      var name = document.createElement('span');
-      name.setAttribute('class', 'product-details')
-      name.innerText = item.name;
-      carouselCard.appendChild(name);
+    var price = document.createElement('span');
+    price.setAttribute('class', 'product-price');
+    price.innerText = item.price;
+    carouselCard.appendChild(price);
 
-      var price = document.createElement('span');
-      price.setAttribute('class', 'product-price');
-      price.innerText = item.price;
-      carouselCard.appendChild(price);
-
-      carousel.appendChild(carouselCard);
-
-      areaRoot.appendChild(carousel);
+    carousel.appendChild(carouselCard);
+    
+    areaRoot.appendChild(carousel);
   }
 
   const URL = 'https://israel-lopes.github.io/storage/productEletronicos.json';
   fetch(URL)
     .then(resposta => resposta.json())
-    .then(data => data.product['eletroportateis'].map((item) => {
-      createCard(item);
+    .then(data => data.product['eletroportateis'].map((item, index) => {
+      createCard(item, index);
     }))
     .catch(erro => console.error(erro));
 
